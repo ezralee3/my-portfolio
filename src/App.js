cf2a5738814096a9ec4fc7e0b26d1c902f29828c
Ezra Lee <e.lee1@ufl.edu>
2021-05-05 15:17:44 -0400

    redundancy changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useLayoutEffect } from 'react'
+import React from 'react'
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,3 @@
-import SanityClientConstructor from "@sanity/client";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import sanityClient from "../client.js";
